30902fa7de622a54e1bf4e32007a71aa963c272e
cyPark95 <parkcy1007@gmail.com>
2024-01-29 10:42:38 +0900

    Create Algorithm Template

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    private static void input() {
+        ScannerReader sr = new ScannerReader();
+    }
+
+    private static void solution() {
+
+    }
+
+    static class ScannerReader {
+
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        public ScannerReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String getReadLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            return "";
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(getReadLine());
+            }
+
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
