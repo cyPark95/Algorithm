83f71139353acab015013e9eae005487a373280f
ChanYeong <139435149+cyPark95@users.noreply.github.com>
2025-03-01 13:54:26 +0900

    [level 2] Title: H-Index, Time: 1.30 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = citations.length;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            if (answer <= citations[i]) {
+                break;
+            }
+            
+            answer--;
+        }
+        
+        return answer;
+    }
+}
