908ef86fb6387a75a3b9416366a639a76557fdcf
ChanYeong <139435149+cyPark95@users.noreply.github.com>
2025-03-24 14:57:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 5.95 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        Arrays.sort(jobs, Comparator.comparingInt(o -> o[0]));
+
+        int index = 0;
+        int endTime = 0;
+        PriorityQueue<Job> pq = new PriorityQueue<>();
+        while (index < jobs.length || !pq.isEmpty()) {
+            while (index < jobs.length && jobs[index][0] <= endTime) {
+                pq.offer(new Job(index, jobs[index][0], jobs[index][1]));
+                index++;
+            }
+
+            if (pq.isEmpty()) {
+                endTime = jobs[index][0];
+                pq.offer(new Job(index, jobs[index][0], jobs[index][1]));
+                index++;
+            } else {
+                Job job = pq.poll();
+                endTime += job.time;
+                answer += endTime - job.requestTime;
+            }
+        }
+
+        return answer / jobs.length;
+    }
+
+    static class Job implements Comparable<Job> {
+
+        int index;
+        int requestTime;
+        int time;
+
+        public Job(int index, int requestTime, int time) {
+            this.index = index;
+            this.requestTime = requestTime;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Job job) {
+            if (this.time == job.time) {
+                if (this.requestTime == job.requestTime) {
+                    return this.index - job.index;
+                }
+
+                return this.requestTime - job.requestTime;
+            }
+
+            return this.time - job.time;
+        }
+    }
+}
