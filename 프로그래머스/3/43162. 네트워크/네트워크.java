eb530b4db24ba0bb051782825ad94d9c09ac2bee
ChanYeong <139435149+cyPark95@users.noreply.github.com>
2025-08-05 17:22:43 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(computers, visited, n, i);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private void dfs(int[][] computers, boolean[] visited, int n, int node) {
+        visited[node] = true;
+
+        for (int i = 0; i < n; i++) {
+            if (computers[node][i] == 1 && !visited[i]) {
+                dfs(computers, visited, n, i);
+            }
+        }
+    }
+}
