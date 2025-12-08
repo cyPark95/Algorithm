0df04d56c1ccbe124887caf9bded6cda7c63c552
ChanYeong <139435149+cyPark95@users.noreply.github.com>
2025-12-08 21:31:11 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 11.03 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -3,129 +3,86 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] expressions) {
         String[] answer = {};
-
-        List<String[]> complete = new ArrayList<>();
-        List<String[]> problem = new ArrayList<>();
+        List<String> problemExpressions = new ArrayList<>();
+        List<String> validExpressions = new ArrayList<>();
 
         int minBase = 2;
-
         for (String expression : expressions) {
+            if (expression.contains("X")) {
+                problemExpressions.add(expression);
+            } else {
+                validExpressions.add(expression);
+            }
+
             String[] parts = expression.split(" ");
 
-            if (parts[4].contains("X")) {
-                problem.add(parts);
-            } else {
-                complete.add(parts);
-            }
-
             for (int i = 0; i < parts.length; i += 2) {
-                if (parts[i].contains("X")) {
-                    continue;
-                }
-
                 for (char c : parts[i].toCharArray()) {
-                    minBase = Math.max(minBase, (c - '0') + 1);
+                    if (c == 'X') {
+                        continue;
+                    }
+
+                    int num = c - '0';
+                    minBase = Math.max(minBase, num + 1);
                 }
             }
         }
 
-        boolean[] possibleBase = new boolean[10];
-        Arrays.fill(possibleBase, true);
+        boolean[] checkedBase = new boolean[10];
 
-        for (int base = minBase; base <= 9; base++) {
-            for (String[] expression : complete) {
-                if (!isValidNumber(expression[0], base) || !isValidNumber(expression[2], base) || !isValidNumber(expression[4], base)) {
-                    possibleBase[base] = false;
-                    break;
-                }
+        for (String expression : validExpressions) {
+            String[] parts = expression.split(" ");
 
-                int result = calc(expression, base);
-                int expected = toDecimal(expression[4], base);
-
-                if (result != expected) {
-                    possibleBase[base] = false;
-                    break;
-                }
+            for (int base = minBase; base <= 9; base++) {
+                int result = evaluateExpression(parts, base);
+                checkedBase[base] = result == convertToDecimal(parts[4], base);
             }
         }
 
-        answer = new String[problem.size()];
-        for (int i = 0; i < problem.size(); i++) {
-            String[] expression = problem.get(i);
+        String[] answers = new String[problemExpressions.size()];
+        for (int i = 0; i < problemExpressions.size(); i++) {
+            String[] parts = problemExpressions.get(i).split(" ");
             Set<String> resultSet = new HashSet<>();
 
             for (int base = minBase; base <= 9; base++) {
-                if (!possibleBase[base]) {
-                    continue;
+                if (checkedBase[base]) {
+                    int result = evaluateExpression(parts, base);
+
+                    if (result == 0) {
+                        resultSet.add("0");
+                    } else if (result > 0) {
+                        StringBuilder sb = new StringBuilder();
+                        while (result > 0) {
+                            sb.append(result % base);
+                            result /= base;
+                        }
+                        resultSet.add(sb.reverse().toString());
+                    }
                 }
-
-                if (!isValidNumber(expression[0], base) || !isValidNumber(expression[2], base)) {
-                    continue;
-                }
-
-                int result = calc(expression, base);
-                String baseStr = toBase(result, base);
-
-                resultSet.add(baseStr);
             }
 
-            String finalValue = (resultSet.size() == 1)
-                    ? resultSet.iterator().next()
-                    : "?";
-
-            answer[i] = String.format("%s %s %s = %s", expression[0], expression[1], expression[2], finalValue);
+            answers[i] = problemExpressions.get(i).replace("X", resultSet.size() == 1 ? resultSet.iterator().next() : "?");
         }
 
-        return answer;
+        return answers;
     }
 
-    private boolean isValidNumber(String num, int base) {
-        for (char ch : num.toCharArray()) {
-            int d = ch - '0';
-            if (d < 0 || d >= base) {
-                return false;
-            }
+    private int convertToDecimal(String numberString, int base) {
+        int value = 0;
+        for (char c : numberString.toCharArray()) {
+            int digit = c - '0';
+            value = value * base + digit;
         }
-        return true;
+        return value;
     }
 
-    private int toDecimal(String num, int base) {
-        int val = 0;
-        for (char ch : num.toCharArray()) {
-            val = val * base + (ch - '0');
-        }
-        return val;
-    }
-    
-    private int calc(String[] expression, int base) {
-        int num1 = toDecimal(expression[0], base);
-        int num2 = toDecimal(expression[2], base);
+    private int evaluateExpression(String[] parts, int base) {
+        int number1 = convertToDecimal(parts[0], base);
+        int number2 = convertToDecimal(parts[2], base);
 
-        if (expression[1].equals("+")) {
-            return num1 + num2;
+        if (parts[1].equals("+")) {
+            return number1 + number2;
         }
-        return num1 - num2;
-    }
-
-    private String toBase(int value, int base) {
-        if (value == 0) {
-            return "0";
-        }
-
-        if (value < 0) {
-            value = -value;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while (value > 0) {
-            sb.append(value % base);
-            value /= base;
-        }
-
-        if (value < 0) {
-            sb.append('-');
-        }
-        
-        return sb.reverse().toString();
+        return number1 - number2;
     }
 }
